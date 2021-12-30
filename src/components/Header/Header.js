a3f5084395e0e2310ceb738198c0615b5ad9f0fe
Rodrigo Suguimto <rodrigosuguimoto@gmail.com>
2021-12-30 11:53:42 -0300

    Changed header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,20 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { DiAtom } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+          <DiAtom size="3rem"/> <span>Portfolio</span>
+        </a>
+      </Link>
+    </Div1>
+  </Container>
 );
 
 export default Header;
