9db767e6fd0355992c4401e25ab2f44a319718cb
Rodrigo Suguimto <rodrigosuguimoto@gmail.com>
2021-12-30 12:33:03 -0300

    Finishing header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,50 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { DiAtom } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
-          <DiAtom size="3rem"/> <span>Portfolio</span>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20' }}>
+          <DiAtom size="3rem"/> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>
+            Projects
+          </NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>
+            Tech
+          </NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>
+            About
+          </NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/Rodrigo-Suguimoto" target="_blank">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/rodrigosuguimoto/" target="_blank">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
